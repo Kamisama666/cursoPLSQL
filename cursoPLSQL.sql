1dfd77a6b144fbb207c365a00794c51f5edd1a71
Kamisama666 <andaynotemenes@gmail.com>
2013-08-12 13:55:59 +0200

    Correccion de errores sintacticos

--- cursoPLSQL.sql
+++ cursoPLSQL.sql
@@ -12,18 +12,22 @@ En este curso aprenderemos los fundamentos del lenguaje PL/SQL, un lenguaje de p
 diseñado para ser usado con en motor de bases de datos Oracle. Por esta razón solo podrá 
 usarse con esta base de datos y no otra. Para realizar este curso es necesario conocer
 el lenguaje de consultas SQL, el lenguaje de manipulacion DML y algunos conceptos básicos de programación.
+Es preferible que este curso se realice junto con el apoyo de otros recursos adicionales.
 
-Como en cualquier otro lenguaje, las istrucciones de PL/SQL se guardan en archivos de texto plano 
+Como en cualquier otro lenguaje, las instrucciones de PL/SQL se guardan en archivos de texto plano 
 denominados scripts. Para ejercutarlos usaremos la orden:
 	@<ruta_archivo>;
+
 Junto con el curso se adjunta una carpeta llamada "ejecutables" en la que se guardan los scripts usados en el
 curso para que puedan ser directamente ejecutado. A lo largo del curso se indicará el nombre del 
 script usado con la sintaxis.
 	--script: <nombre>
-Tambien encontraras varioes ejercicios para realizar en la carpeta ejercicios asi como su solucion.
+
+Tambien encontraras varios ejercicios para realizar en la carpeta ejercicios asi como su solucion.
 A lo largo del curso se indicara cuando se puede realizar cada uno con la sintaxis:
-	--ejercio: <nombre>
-Esa guía ha sido realizada usando Oracle 11g r2 en un CentOS 6.4
+	--ejercicio: <nombre>
+
+Este curso ha sido realizada usando Oracle 11g r2 en un CentOS 6.4
 
 */
 
@@ -81,7 +85,7 @@ END; /*con end terminamos el programa*/
 /*-------------------------------------------------------------------------------------------------------------
 	3. Mostrar datos por pantalla
 	Para poder mostrar el contenido de una variable o una cadena de texto
-	usamos el comando
+	usamos el comando:
 	dbms_output.put_line(dato a mostar);
 */
 
